217a6973424ae6c0a8edaf7633ba14105e988978
Siebe Hoogenboom <shoogenb@f0r1s11.codam.nl>
2022-01-25 16:59:07 +0100

    added the parser.c file where the input should be parsed

---
+++ src/parser.c
@@ -0,0 +1,25 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        ::::::::            */
+/*   parser.c                                           :+:    :+:            */
+/*                                                     +:+                    */
+/*   By: shoogenb <shoogenb@student.codam.nl>         +#+                     */
+/*                                                   +#+                      */
+/*   Created: 2022/01/25 16:57:28 by shoogenb      #+#    #+#                 */
+/*   Updated: 2022/01/25 16:58:37 by shoogenb      ########   odam.nl         */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "minishell.h"
+
+/*
+ * This function parses the input
+ * from the readline function.
+ * it needs to handle double and single quotes
+ * $ sign and also all the built-ins
+ * like echo, cd, pwd etc.
+ */
+void	parse_input(char *input)
+{
+	
+}

3e41431cbfe941bb628a2cea48ed7b1ea8849be4
Siebe Hoogenboom <shoogenb@f0r1s11.codam.nl>
2022-01-25 14:33:05 +0100

    added a to do txt file and made some slight changes

--- src/minishell.c
+++ src/minishell.c
@@ -6,7 +6,7 @@
 /*   By: shoogenb <shoogenb@student.codam.nl>         +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2022/01/24 12:13:09 by shoogenb      #+#    #+#                 */
-/*   Updated: 2022/01/25 14:16:07 by shoogenb      ########   odam.nl         */
+/*   Updated: 2022/01/25 14:32:07 by shoogenb      ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -64,18 +64,21 @@ void	check_input(char *input, char **envp)
  * commands. Won't work yet with pipe.
  * Also the return value required for the echo $? needs to be done
  */
-void	minishell_thing(char *input, char **envp)
+int	minishell_thing(char *input, char **envp)
 {
 	pid_t	child_pid;
 	int		status;
 
 	child_pid = fork();
 	if (child_pid < 0)
-		return (perror("FORK: "));
+	{
+		perror("FORK: ");
+		return (1);
+	}
 	if (child_pid == 0)
 		check_input(input, envp);
 	waitpid(child_pid, &status, 0);
-	//exit(WEXITSTATUS(status));
+	return (WEXITSTATUS(status));
 }
 
 /*
@@ -84,12 +87,16 @@ void	minishell_thing(char *input, char **envp)
  */
 void	signal_handle_function(int sig)
 {
-	if (sig == 2)
+	if (sig == SIGINT)
 	{
 		printf("\n");
 		rl_on_new_line();
 		rl_replace_line("", 0);
 		rl_redisplay();
+	}
+	if (sig == SIGQUIT)
+	{
+		printf("ctrl-c \n");
 	}
 }
 

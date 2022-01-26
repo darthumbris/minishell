28be40f721d77eb6a5e257d69a676ae631873c56
Siebe Hoogenboom <shoogenb@f0r1s11.codam.nl>
2022-01-26 14:15:39 +0100

    made sure that the built-ins only execute when its the command (won't execute cd when cdb is typed for example)

--- src/parser.c
+++ src/parser.c
@@ -6,7 +6,7 @@
 /*   By: shoogenb <shoogenb@student.codam.nl>         +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2022/01/25 16:57:28 by shoogenb      #+#    #+#                 */
-/*   Updated: 2022/01/26 14:04:39 by shoogenb      ########   odam.nl         */
+/*   Updated: 2022/01/26 14:14:34 by shoogenb      ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,49 +14,50 @@
 
 void	cd_function(char *input)
 {
-	if (input)
-		printf("entered cd function\n");
+	if (input && *input == ' ')
+		printf("entered cd function correctly\n");
+	printf("\n%d\n", ' ');
 	return ;
 }
 
 void	pwd_function(char *input)
 {
-	if (input)
+	if (input && *input == ' ')
 		printf("entered pwd function\n");
 	return ;
 }
 
 void	export_function(char *input)
 {
-	if (input)
+	if (input && *input == ' ')
 		printf("entered export function\n");
 	return ;
 }
 
 void	unset_function(char *input)
 {
-	if (input)
+	if (input && *input == ' ')
 		printf("entered unset function\n");
 	return ;
 }
 
 void	env_function(char *input)
 {
-	if (input)
+	if (input && *input == ' ')
 		printf("entered env function\n");
 	return ;
 }
 
 void	exit_function(char *input)
 {
-	if (input)
+	if (input && *input == ' ')
 		printf("entered exit function\n");
 	return ;
 }
 
 void	echo_function(char *input)
 {
-	if (input)
+	if (input && *input == ' ')
 		printf("entered echo function\n");
 	return ;
 }
@@ -69,6 +70,7 @@ void	echo_function(char *input)
  * like echo, cd, pwd etc.
  * need to also take into account whitespace
  * before the command (i.e.:       pwd should still execute)
+ * need to also check that it won't work with : cdb or something
  */
 void	parse_input(char *input, char **envp)
 {
@@ -100,7 +102,7 @@ void	parse_input(char *input, char **envp)
 	while (keys[i])
 	{
 		if (ft_strncmp(input, keys[i], ft_strlen(keys[i])) == 0)
-			return (function[i](input));
+			return (function[i](input + ft_strlen(keys[i])));
 		i++;
 	}
 	minishell_thing(input, envp);

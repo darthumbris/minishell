8d184bbf17d28505c7fba84a4e1a6348db30075c
Abdoulaye Ba <abba@f0r3s24.codam.nl>
2022-02-03 20:53:44 +0100

    add more token in dispatch table so we can check them easily

--- src/token.c
+++ src/token.c
@@ -6,7 +6,7 @@
 /*   By: abba <abba@student.codam.nl>                 +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2022/02/01 13:45:46 by abba          #+#    #+#                 */
-/*   Updated: 2022/02/02 13:45:38 by abba          ########   odam.nl         */
+/*   Updated: 2022/02/03 20:52:44 by abba          ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -122,53 +122,193 @@ char	*str_copy(char *str, int end, int start)
 	return (output);
 }
 
-char	**split_output(char *str, t_check *check)
+int	quote_count(char *str)
 {
-	char	**output;
-	int		i;
-	int		j;
+	int len;
+	int	i;
 
 	i = 0;
-	j = 0;
-	output = ft_calloc(check->len + 1, sizeof(char *));
-	if (!output)
-		return (NULL);
-	init_check(check);
+	len = 0;
 	while (str[i])
 	{
-		if (str[i] == '\'')
-			single_quote(check);
-		if (str[i] == '\"')
-			double_quotes(check);
-		else if ((str[i] == ' ' && !check->single_quote\
-			&& !check->double_quotes) || (str[i + 1] == '\0'))
-		{
-			output[j] = str_copy(str, i, check->position);
-			check->position = i + 1;
-			j++;
-		}
+		if (str[i] == '\'' || str[i] == '\"')
+			len++;
 		i++;
-	}	
-	return (output);
+	}
+	return (len);
+}
+
+
+// string =  echo "hello 'ho  '   " f
+
+
+// void	ft_test(char **output, char *str, int *j)
+// {
+// 	int		i;
+// 	int		k;
+// 	int		len;
+// 	char	*quote;
+ 
+// 	i = 0;
+// 	k = 0;
+// 	len = quote_count(str);
+// 	quote = ft_calloc(len + 1, sizeof(char));
+// 	if (!quote)
+// 		return ;
+// 	while (str[i])
+// 	{
+// 		if (str[i] == '\'' || str[i] == '\"') // TO DOO TOMMMOROW FINISH EXPANSION PART echo hello " 'yoou'"   
+// 		{
+// 			quote[k] = str[i];
+// 			k++;
+// 		}
+// 	}
+	
+// }
+
+// char	*expansion(char **test)
+// {
+// 	int	i;
+// 	int	j;
+// 	char	*output;
+
+// 	i = 0;
+// 	j = 0;
+// 	while (test[i])
+// 	{
+// 		ft_test(&output, test[i], &
+// 		j);
+// 		i++;
+// 	}
+// }
+
+typedef struct s_quote
+{
+	char	quote;
+	char	*(*t_function_pointer)(char *input, int *i);
+}			t_quote;
+
+int	ft_count(char *str, int i, char c)
+{
+	int	len;
+
+	len = 0;
+	while (str[i] != c)
+	{
+		len++;
+		i++;
+	}
+	return (len);
+}
+
+char	*single_function(char *input, int *i)
+{
+	int			j;
+	char		*tmp;
+
+	(*i)++;
+	j = 0;
+	tmp = ft_calloc(ft_count(input, *i, '\'') + 1, sizeof(char));
+	if (!tmp)
+		return (0);
+	while (input[*i] && input[*i] != '\'')
+	{
+		tmp[j] = input[*i];
+		(*i)++;
+		j++;
+	}
+	return (tmp);
+}
+
+char	*double_function(char *input, int *i)
+{
+	int			j;
+	char		*tmp;
+
+	(*i)++;
+	j = 0;
+	tmp = ft_calloc(ft_count(input, *i, '\"') + 1, sizeof(char));
+	if (!tmp)
+		return (0);
+	while (input[*i] && input[*i] != '\"')
+	{
+		tmp[j] = input[*i];
+		(*i)++;
+		j++;
+	}
+	return (tmp);
+}
+
+int	ft_count1(char *str, int i)
+{
+	int	len;
+
+	len = 0;
+	while (str[i] && str[i] != '\'' )
+	{
+		if (str[i] == '"')
+			break ;
+		len++;
+		i++;
+	}
+	return (len);
+}
+
+char	*normal_function(char *input, int *i)
+{
+	int		j;
+	char	*tmp;
+
+	j = 0;
+	tmp = ft_calloc(ft_count1(input, *i) + 1, sizeof(char));
+	if (!tmp)
+		return (0);
+	while (input[*i] && input[*i] != '\'')
+	{
+		if (input[*i] == '"')
+			break ;
+		tmp[j] = input[*i];
+		(*i)++;
+		j++;
+	}
+	(*i)--;
+	return (tmp);
+}
+
+char	*state_macchine(char *output)
+{
+	int							i;
+	char						*str;
+	const t_quote				function[] = {
+		{'\'', &single_function},
+		{'"', &double_function},
+		{' ', &normal_function},
+		{0, NULL}
+	};
+	
+	i = 0;
+	str = ft_strdup("");
+	while(output[i])
+	{
+		if (output[i] == function[0].quote)
+			str = ft_strjoin(str, function[0].t_function_pointer(output, &i));
+		else if (output[i] == function[1].quote)
+			str = ft_strjoin(str, function[1].t_function_pointer(output, &i));
+		else
+			str = ft_strjoin(str, function[2].t_function_pointer(output, &i));
+		i++;
+	}
+	return (str);
 }
 
 void	tokenizer(char *output)
 {
 	t_check	check;
-	char	**test;
-	int i = 0;
+	char	*test;
 
 	init_check(&check);
 	output = remove_white_spaces(output, &check);
-	test = split_output(output, &check);
-	while (test[i])
-	{
-		printf("test: <%s>\n", test[i]);
-		free(test[i]);
-		i++;
-	}
+	test = state_macchine(output);
+	printf("output = %s\n", test);
 	free(test);
 }
-
-// out = cd "hello"yoou '    d' hello
-// free
